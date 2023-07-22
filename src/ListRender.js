439ce025161b53cfe6127eb112a52ad6d708ce9a
Niranjan Zambare <niranjan.zambare@tietoevry.com>
2023-07-22 23:02:11 +0530

    React learning code

---
+++ src/ListRender.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+
+const people = [{
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A'
+  }, {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa'
+  }, {
+    id: 2,
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+    accomplishment: 'electromagnetism theory',
+    imageId: 'bE7W1ji'
+  }, {
+    id: 3,
+    name: 'Percy Lavon Julian',
+    profession: 'astrophysicist',
+    accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+    imageId: 'IOjWm71'
+  }, {
+    id: 4,
+    name: 'Subrahmanyan Chandrasekhar',
+    profession: 'astrophysicist',
+    accomplishment: 'white dwarf star mass calculations',
+    imageId: 'lrWQx8l'
+  }];
+  
+
+const ListRender = () => {
+
+const astrophysicist= people.filter(x=>x.profession==='astrophysicist');
+const list=astrophysicist.map(x=>
+    <li>
+    <span>{x.name}</span>
+    </li>
+
+);
+  return <ul>{list}</ul>;
+}
+
+
+export default ListRender
---
+++ src/MyButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MyButton = () => {
+  return (
+    <div>
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+
+    
+    </div>
+  )
+}
+
+export default MyButton
